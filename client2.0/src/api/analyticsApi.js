f5201f94285416e6dca52d09c86c553294ca76d5
Learnerbypassion <callmedad002022@gmail.com>
2026-06-30 21:54:20 +0530

    Updated folder

--- client2.0/src/api/analyticsApi.js
+++
@@ -1,8 +0,0 @@
-import API from './axios';
-
-export const getStudentStats = () => API.get('/analytics/student');
-export const getInstructorStats = () => API.get('/analytics/instructor');
-export const getAdminStats = () => API.get('/analytics/admin');
-export const getNotifications = () => API.get('/notifications');
-export const markNotificationRead = (id) => API.put(`/notifications/${id}/read`);
-export const markAllNotificationsRead = () => API.put('/notifications/read-all');
--- client2.0/src/api/certificateApi.js
+++
@@ -1,6 +0,0 @@
-import API from './axios';
-
-export const generateCertificate = (courseId) => API.post('/certificates/generate', { courseId });
-export const getMyCertificates = () => API.get('/certificates/me');
-export const getCertificate = (id) => API.get(`/certificates/${id}`);
-export const verifyCertificate = (certificateId) => API.get(`/certificates/verify/${certificateId}`);
